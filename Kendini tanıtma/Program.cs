169675a7b6667b8f42dc060e790c655e1068a8f1
OnurGurkana <120009502+OnurGurkana@users.noreply.github.com>
2023-01-05 15:57:15 +0300

    Add files via upload

---
+++ Kendini tanıtma/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Değişkenler ve Sabitler
+***********************
+veri tipi değişkenadı
+
+
+*/
+string adi = "Onur";
+int yas = 16;
+long telefon = 2121265289263;
+double kilo=90.30;
+bool evliMi=false;
+
+// Console.WriteLine("Merhaba " + adi);
+Console.WriteLine("Merhaba ben {0}, yaşım {1} telefon numaram {2}, kilom {3} medeni durumum {4}", adi, yas, telefon, kilo, evliMi?"Evli":"Bekar");
+
+
+
+
